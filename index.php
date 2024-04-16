18c67efab2ee227dd95fd61ac2c00090ee30e215
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-04-16 17:01:59 +0200

    Fixed Password Generator

--- index.php
+++ index.php
@@ -1,29 +1,40 @@
 <?php
-include_once __DIR__ . '/components/head.php';
 
-$passwordLength = $_GET["passwordLengthInput"];
-function passwordGeneration($passwordLength) {
+$password = "";
+$passwordLength = $_GET['passwordLengthInput'];
+if(!isset($_GET['passwordLengthInput'])) {
+    $passwordLength = '0';
+} else {
+    $password = passwordGeneration($passwordLength, $password);
+};
+//risolto il casino con password, il problema principale era lo scope della funzione
+
+function passwordGeneration($length, $resultString) {
         
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?&%$<>^+-*/()[]{}@#_';
-    $password = '';
      
-    for ($i = 0; $i < $passwordLength; $i++) {
+    for ($i = 0; $i <= $length; $i++) {
         $index = rand(0, strlen($characters) - 1);
-        $password .= $characters[$index];
+        $resultString .= $characters[$index];
     }
      
-    return $password;
+    return $resultString;
 } 
 
+include_once __DIR__ . '/components/head.php';
 ?>
 
 <body>
-    <form action="index.php" method="GET"> 
+    <form action="index.php" method="GET" class=""> 
     <div class="mb-3">
-        <label for="passwordLengthInput" class="form-label">Scegliere lunghezza password</label>
+        <label class="form-label">Scegliere lunghezza password</label>
         <input type="number" class="form-control" name="passwordLengthInput"  id="" placeholder="" min="5" max="20">
     </div>
     </form>
+
+    <p> "La lunghezza della password è: <?php echo $passwordLength?>"</p>
+    <p> "La password generata automaticamente è: <?php echo $password?>"</p>
+    
 </body>
 
 </html>
